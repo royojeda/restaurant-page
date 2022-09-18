9ee3d0fa9086387c70874e52179d92c10f6e4377
Dennis Ojeda <royojeda1@gmail.com>
2022-09-19 01:50:15 +0800

    Update page layout

--- src/tabs.js
+++ src/tabs.js
@@ -1,51 +1,72 @@
-const home = () => {
-  const content = document.querySelector("#content")
+const home = (content) => {
   content.innerHTML = `
-    <button type="button" class="home rounded shadow border p-2 bg-black text-white">
-      Home
-    </button>
+    <div class="bg-neutral-800 w-screen h-screen p-2 flex flex-col gap-2">
+      <div class="grid grid-cols-3 gap-2">
+        <button type="button" class="home rounded shadow border border-neutral-900 p-2 bg-neutral-600 text-white font-semibold">
+          Home
+        </button>
 
-    <button type="button" class="menu rounded shadow border p-2 bg-white text-black">
-      Menu
-    </button>
+        <button type="button" class="menu rounded shadow border border-neutral-900 p-2 bg-neutral-700 text-white">
+          Menu
+        </button>
 
-    <button type="button" class="contact rounded shadow border p-2 bg-white text-black">
-      Contact
-    </button>
+        <button type="button" class="contact rounded shadow border border-neutral-900 p-2 bg-neutral-700 text-white">
+          Contact
+        </button>
+      </div>
+
+      <div class="flex-1">
+
+      </div>
+    </div>
   `
 }
 
-const menu = () => {
-  const content = document.querySelector("#content")
+const menu = (content) => {
   content.innerHTML = `
-    <button type="button" class="home rounded shadow border p-2 bg-white text-black">
-      Home
-    </button>
+    <div class="bg-neutral-800 w-screen h-screen p-2 flex flex-col gap-2">
+      <div class="grid grid-cols-3 gap-2">
+        <button type="button" class="home rounded shadow border border-neutral-900 p-2 bg-neutral-700 text-white">
+          Home
+        </button>
 
-    <button type="button" class="menu rounded shadow border p-2 bg-black text-white">
-      Menu
-    </button>
+        <button type="button" class="menu rounded shadow border border-neutral-900 p-2 bg-neutral-600 text-white font-semibold">
+          Menu
+        </button>
 
-    <button type="button" class="contact rounded shadow border p-2 bg-white text-black">
-      Contact
-    </button>
+        <button type="button" class="contact rounded shadow border border-neutral-900 p-2 bg-neutral-700 text-white">
+          Contact
+        </button>
+      </div>
+
+      <div class="flex-1">
+
+      </div>
+    </div>
   `
 }
 
-const contact = () => {
-  const content = document.querySelector("#content")
+const contact = (content) => {
   content.innerHTML = `
-    <button type="button" class="home rounded shadow border p-2 bg-white text-black">
-      Home
-    </button>
+    <div class="bg-neutral-800 w-screen h-screen p-2 flex flex-col gap-2">
+      <div class="grid grid-cols-3 gap-2">
+        <button type="button" class="home rounded shadow border border-neutral-900 p-2 bg-neutral-700 text-white">
+          Home
+        </button>
 
-    <button type="button" class="menu rounded shadow border p-2 bg-white text-black">
-      Menu
-    </button>
+        <button type="button" class="menu rounded shadow border border-neutral-900 p-2 bg-neutral-700 text-white">
+          Menu
+        </button>
 
-    <button type="button" class="contact rounded shadow border p-2 bg-black text-white">
-      Contact
-    </button>
+        <button type="button" class="contact rounded shadow border border-neutral-900 p-2 bg-neutral-600 text-white font-semibold">
+          Contact
+        </button>
+      </div>
+
+      <div class="flex-1">
+
+      </div>
+    </div>
   `
 }
 
